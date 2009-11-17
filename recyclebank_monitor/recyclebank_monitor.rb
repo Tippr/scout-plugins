5b583f3fe0fdc4bf594f9de12d4b794b345c1622
Steve Sloan <steve@finagle.org>
2009-11-17 10:19:18 -0800

    Fixed RecycleBank monitoring for new RB user schema.

--- recyclebank_monitor/recyclebank_monitor.rb
+++ recyclebank_monitor/recyclebank_monitor.rb
@@ -25,10 +25,7 @@ class RecyclebankMonitor < Scout::Plugin
     res = mysql.query "SELECT COUNT(*), SUM(amount)
                          FROM recycle_bank_transfers LEFT JOIN users
                            ON recycle_bank_transfers.user_id = users.id
-LEFT JOIN recycle_bank_users
-ON recycle_bank_users.user_id = users.id
-WHERE recycle_bank_users.account IS NULL"
-#                        WHERE users.recycle_bank_account IS NULL"
+                        WHERE users.recycle_bank_account IS NULL"
     row = res.fetch_row
     stats['unclaimed'], stats['unclaimed.amount'] = row[0].to_i, row[1].to_i
     res.free
@@ -37,11 +34,8 @@ WHERE recycle_bank_users.account IS NULL"
     res = mysql.query "SELECT gconomy_state, COUNT(*), SUM(amount)
                          FROM recycle_bank_transfers LEFT JOIN users
                            ON recycle_bank_transfers.user_id = users.id
-LEFT JOIN recycle_bank_users
-ON recycle_bank_users.user_id = users.id
-WHERE recycle_bank_users.account IS NOT NULL
+                        WHERE users.recycle_bank_account IS NOT NULL
                      GROUP BY recycle_bank_transfers.gconomy_state"
-#                        WHERE users.recycle_bank_account IS NOT NULL
     while row = res.fetch_row
       stats[row[0] || ''], stats["#{row[0]}.amount"] = row[1].to_i, row[2].to_i
     end
@@ -51,12 +45,9 @@ WHERE recycle_bank_users.account IS NOT NULL
     res = mysql.query "SELECT gconomy_state, TIME_TO_SEC( TIMEDIFF(UTC_TIMESTAMP, MIN(recycle_bank_transfers.updated_at)) ) / (60 * 60.0)
                          FROM recycle_bank_transfers LEFT JOIN users
                            ON recycle_bank_transfers.user_id = users.id
-LEFT JOIN recycle_bank_users
-ON recycle_bank_users.user_id = users.id
-WHERE recycle_bank_users.account IS NOT NULL
                           AND gconomy_state IN ('created','submitted')
-                     GROUP BY recycle_bank_transfers.gconomy_state";
-#                        WHERE users.recycle_bank_account IS NULL"
+                        WHERE users.recycle_bank_account IS NULL
+                     GROUP BY recycle_bank_transfers.gconomy_state"
     while row = res.fetch_row
       stats["#{row[0]}.oldest"] = row[1].to_i
     end
